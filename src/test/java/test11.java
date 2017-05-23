976e659cdf83ff4c0df411d39bf312eb1a660d14
yulydd <2540258825@qq.com>
2017-05-23 11:33:50 +0800

    第一次提交项目

---
+++ src/test/java/test11.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/5/18.
+ */
+public class test11 {
+
+    @Test
+    public void test1() {
+        System.out.println("老夫聊发少年狂");
+    }
+}
